fd9abcc7ea264c846f9af554483d0390b05507e3
Andreas Koepsel <andreas.koepsel@bisdn.de>
2015-07-23 11:50:27 +0200

    Fixes issue 13

--- src/rofl/common/openflow/cofgroupmod.h
+++ src/rofl/common/openflow/cofgroupmod.h
@@ -181,7 +181,7 @@ public:
 			default:
 				throw eBadVersion();
 			}
-			os << "group-id:" << (int)ge.get_group_id() << " >" << std::endl;
+			os << "group-id: 0x" << std::hex << (unsigned int)ge.get_group_id() << std::dec << " >" << std::endl;
 			os << indent(2) << "<buckets: >" << std::endl;
 			indent i(4);
 			os << ge.buckets;
