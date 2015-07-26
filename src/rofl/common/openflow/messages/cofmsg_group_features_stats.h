9d52a7461dc62a609d9eaedfe8ae6e86b00d9eb8
Andreas Koepsel <andreas.koepsel@bisdn.de>
2015-07-26 17:39:51 +0200

    revised class cofmsg_group_features_stats

--- src/rofl/common/openflow/messages/cofmsg_group_features_stats.h
+++ src/rofl/common/openflow/messages/cofmsg_group_features_stats.h
@@ -1,8 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /*
  * cofmsg_group_features_stats.h
  *
  *  Created on: 18.03.2013
- *      Author: andi
+ *  Revised on: 26.07.2015
+ *      Author: Andreas Koepsel
  */
 
 #ifndef COFMSG_GROUP_FEATURES_STATS_H_
@@ -18,97 +23,73 @@ namespace openflow {
  *
  */
 class cofmsg_group_features_stats_request :
-	public cofmsg_stats_request
+		public cofmsg_stats_request
 {
-private:
-
 public:
 
-
-	/** constructor
+	/**
 	 *
 	 */
-	cofmsg_group_features_stats_request(
-			uint8_t of_version = 0,
-			uint32_t xid = 0,
-			uint16_t flags = 0);
-
+	virtual
+	~cofmsg_group_features_stats_request();
 
 	/**
 	 *
 	 */
 	cofmsg_group_features_stats_request(
-			cofmsg_group_features_stats_request const& stats);
+			uint8_t version = rofl::openflow::OFP_VERSION_UNKNOWN,
+			uint32_t xid = 0,
+			uint16_t flags = 0);
 
+	/**
+	 *
+	 */
+	cofmsg_group_features_stats_request(
+			const cofmsg_group_features_stats_request& msg);
 
 	/**
 	 *
 	 */
 	cofmsg_group_features_stats_request&
 	operator= (
-			cofmsg_group_features_stats_request const& stats);
+			const cofmsg_group_features_stats_request& msg);
 
-
-	/** destructor
-	 *
-	 */
-	virtual
-	~cofmsg_group_features_stats_request();
-
+public:
 
 	/**
-	 *
-	 */
-	cofmsg_group_features_stats_request(cmemory *memarea);
-
-
-	/** reset packet content
-	 *
-	 */
-	virtual void
-	reset();
-
-
-	/**
-	 *
-	 */
-	virtual uint8_t*
-	resize(size_t len);
-
-
-	/** returns length of packet in packed state
 	 *
 	 */
 	virtual size_t
 	length() const;
 
+	/**
+	 *
+	 */
+	virtual void
+	pack(
+			uint8_t *buf = (uint8_t*)0, size_t buflen = 0);
 
 	/**
 	 *
 	 */
 	virtual void
-	pack(uint8_t *buf = (uint8_t*)0, size_t buflen = 0);
-
-
-	/**
-	 *
-	 */
-	virtual void
-	unpack(uint8_t *buf, size_t buflen);
-
-
-	/** parse packet and validate it
-	 */
-	virtual void
-	validate();
+	unpack(
+			uint8_t *buf, size_t buflen);
 
 public:
 
 	friend std::ostream&
-	operator<< (std::ostream& os, cofmsg_group_features_stats_request const& msg) {
-		os << dynamic_cast<cofmsg const&>( msg );
+	operator<< (std::ostream& os, const cofmsg_group_features_stats_request& msg) {
+		os << dynamic_cast<const cofmsg_stats_request&>( msg );
 		os << indent(2) << "<cofmsg_group_features_stats_request >" << std::endl;
 		return os;
+	};
+
+	virtual std::string
+	str() const {
+		std::stringstream ss;
+		ss << cofmsg::str() << "-Group-Features-Stats-Request- " << " ";
+		return ss.str();
 	};
 };
 
@@ -118,124 +99,97 @@ public:
  *
  */
 class cofmsg_group_features_stats_reply :
-	public cofmsg_stats_reply
+		public cofmsg_stats_reply
 {
-private:
-
-	cofgroup_features_stats_reply 		group_features_stats;
-
-	union {
-		uint8_t*								ofhu_group_features_stats;
-		struct openflow12::ofp_group_features_stats*		ofhu12_group_features_stats;
-		// TODO: OF1.3
-	} ofhu;
-
-#define ofh_group_features_stats   			ofhu.ofhu_group_features_stats
-#define ofh12_group_features_stats 			ofhu.ofhu12_group_features_stats
-// TODO OF1.3
-
 public:
 
-
-	/** constructor
+	/**
 	 *
 	 */
-	cofmsg_group_features_stats_reply(
-			uint8_t of_version = 0,
-			uint32_t xid = 0,
-			uint16_t flags = 0,
-			cofgroup_features_stats_reply const& group_features_stats = cofgroup_features_stats_reply());
-
+	virtual
+	~cofmsg_group_features_stats_reply();
 
 	/**
 	 *
 	 */
 	cofmsg_group_features_stats_reply(
-			cofmsg_group_features_stats_reply const& stats);
+			uint8_t version = 0,
+			uint32_t xid = 0,
+			uint16_t flags = 0,
+			const rofl::openflow::cofgroup_features_stats_reply& group_features_stats = rofl::openflow::cofgroup_features_stats_reply());
 
+	/**
+	 *
+	 */
+	cofmsg_group_features_stats_reply(
+			const cofmsg_group_features_stats_reply& msg);
 
 	/**
 	 *
 	 */
 	cofmsg_group_features_stats_reply&
 	operator= (
-			cofmsg_group_features_stats_reply const& stats);
+			const cofmsg_group_features_stats_reply& msg);
 
-
-	/** destructor
-	 *
-	 */
-	virtual
-	~cofmsg_group_features_stats_reply();
-
+public:
 
 	/**
-	 *
-	 */
-	cofmsg_group_features_stats_reply(cmemory *memarea);
-
-
-	/** reset packet content
-	 *
-	 */
-	virtual void
-	reset();
-
-
-	/**
-	 *
-	 */
-	virtual uint8_t*
-	resize(size_t len);
-
-
-	/** returns length of packet in packed state
 	 *
 	 */
 	virtual size_t
 	length() const;
 
+	/**
+	 *
+	 */
+	virtual void
+	pack(
+			uint8_t *buf = (uint8_t*)0, size_t buflen = 0);
 
 	/**
 	 *
 	 */
 	virtual void
-	pack(uint8_t *buf = (uint8_t*)0, size_t buflen = 0);
-
-
-	/**
-	 *
-	 */
-	virtual void
-	unpack(uint8_t *buf, size_t buflen);
-
-
-	/** parse packet and validate it
-	 */
-	virtual void
-	validate();
-
+	unpack(
+			uint8_t *buf, size_t buflen);
 
 public:
 
+	/**
+	 *
+	 */
+	const cofgroup_features_stats_reply&
+	get_group_features_stats() const
+	{ return group_features_stats; };
 
 	/**
 	 *
 	 */
 	cofgroup_features_stats_reply&
-	get_group_features_stats();
-
+	set_group_features_stats()
+	{ return group_features_stats; };
 
 public:
 
 	friend std::ostream&
-	operator<< (std::ostream& os, cofmsg_group_features_stats_reply const& msg) {
-		os << dynamic_cast<cofmsg const&>( msg );
+	operator<< (std::ostream& os, const cofmsg_group_features_stats_reply& msg) {
+		os << dynamic_cast<const cofmsg_stats_reply&>( msg );
 		os << indent(2) << "<cofmsg_group_features_stats_reply >" << std::endl;
 		indent i(4);
 		os << msg.group_features_stats;
 		return os;
 	};
+
+	virtual std::string
+	str() const {
+		std::stringstream ss;
+		ss << cofmsg::str() << "-Group-Features-Stats-Reply- " << " ";
+		return ss.str();
+	};
+
+private:
+
+	cofgroup_features_stats_reply 		group_features_stats;
 };
 
 } // end of namespace openflow
