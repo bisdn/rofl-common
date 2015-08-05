467f10a3c70ebeb0d2f0c8ae4d08d209521e1b01
Andreas Koepsel <andreas.koepsel@bisdn.de>
2015-08-05 17:38:07 +0200

    new test: cofmsg_meter_features_stats

---
+++ test/rofl/common/openflow/messages/cofmsggroupstats/cofmsggroupstatstest.hpp
@@ -0,0 +1,53 @@
+/*
+ * cofmsggroupstatstest.hpp
+ *
+ *  Created on: Apr 26, 2015
+ *      Author: andi
+ */
+
+#ifndef TEST_SRC_ROFL_COMMON_OPENFLOW_MESSAGES_COFMSGGROUPSTATS_TEST_HPP_
+#define TEST_SRC_ROFL_COMMON_OPENFLOW_MESSAGES_COFMSGGROUPSTATS_TEST_HPP_
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "rofl/common/openflow/messages/cofmsg_group_stats.h"
+#include "rofl/common/cmemory.h"
+
+class cofmsggroupstatstest :
+		public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE( cofmsggroupstatstest );
+	CPPUNIT_TEST( testRequest12 );
+	CPPUNIT_TEST( testRequest13 );
+	CPPUNIT_TEST( testReply12 );
+	CPPUNIT_TEST( testReply13 );
+	CPPUNIT_TEST( testRequestParser12 );
+	CPPUNIT_TEST( testRequestParser13 );
+	CPPUNIT_TEST( testReplyParser12 );
+	CPPUNIT_TEST( testReplyParser13 );
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+	void tearDown();
+
+public:
+	void testRequest12();
+	void testRequest13();
+	void testReply12();
+	void testReply13();
+	void testRequestParser12();
+	void testRequestParser13();
+	void testReplyParser12();
+	void testReplyParser13();
+
+private:
+
+	void testRequest(
+			uint8_t version, uint8_t type, uint32_t xid, uint16_t stats_type, uint16_t stats_flags);
+	void testReply(
+			uint8_t version, uint8_t type, uint32_t xid, uint16_t stats_type, uint16_t stats_flags);
+};
+
+#endif /* TEST_SRC_ROFL_COMMON_OPENFLOW_MESSAGES_COFMSGGROUPSTATS_TEST_HPP_ */
