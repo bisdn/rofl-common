37f52faeb839d73dfd8c38c11f8ba403be105b3a
Andreas Koepsel <andreas.koepsel@bisdn.de>
2015-09-28 19:14:41 +0200

    new test for class crofqueue

---
+++ test/rofl/common/crofqueue/crofqueuetest.hpp
@@ -0,0 +1,33 @@
+/*
+ * crofqueuetest.hpp
+ *
+ *  Created on: Apr 26, 2015
+ *      Author: andi
+ */
+
+#ifndef TEST_SRC_ROFL_COMMON_OPENFLOW_MESSAGES_COFMSGAGGRSTATS_TEST_HPP_
+#define TEST_SRC_ROFL_COMMON_OPENFLOW_MESSAGES_COFMSGAGGRSTATS_TEST_HPP_
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "rofl/common/crofqueue.h"
+
+class crofqueuetest :
+		public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE( crofqueuetest );
+	CPPUNIT_TEST( test1 );
+	CPPUNIT_TEST( test2 );
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+	void tearDown();
+
+public:
+	void test1();
+	void test2();
+};
+
+#endif /* TEST_SRC_ROFL_COMMON_OPENFLOW_MESSAGES_COFMSGAGGRSTATS_TEST_HPP_ */
